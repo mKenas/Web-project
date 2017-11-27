686ecd6d74be87ee990919931ff0359da2618871
Mohamad Kenas <m.kenas91@gmail.com>
2017-11-27 16:54:09 +0100

    Add footer , fix the mobile img, add intro sec, add how it works

--- js/js.js
+++ js/js.js
@@ -8,12 +8,80 @@
 }, 27500
   );
 
-// This show change the class name of the navbar to apply the mobile style
+// This function changes the class name of the navbar to apply the mobile style
   function navbarMobile() {
     var x = document.getElementById("navbar");
-    if (x.className === "navbar") {
-        x.className += " mobileNavBar";
+
+    if (x.className === "navbar" ) {
+        x.className = "navbar mobileNavBar";
     } else {
         x.className = "navbar";
     }
+
+
+
+
 }
+
+
+
+function replayVideo(){
+  var icon = document.getElementById("replay-icon");
+  var replayText = document.getElementById("play-again-txt");
+  var video = document.getElementById("save-sharing-video");
+  if (video.paused){
+      video.currentTime = 0;
+      video.play();
+      icon.style.display = "none";
+      replayText.style.display = "none";
+
+
+}
+
+}
+function showReplayText(){
+var icon = document.getElementById("replay-icon");
+var replayText = document.getElementById("play-again-txt");
+icon.style.display = "block";
+replayText.style.display = "block";
+
+}
+
+
+
+window.onscroll = function() {
+
+}
+
+
+function fadeIn() {
+
+    var h1 = document.getElementById("intro-section-h1");
+    var introTxt = document.getElementById("intro-section-text");
+    h1.className = "intro-h1-animation";
+    introTxt.className = "intro-animation-txt";
+
+}
+/*
+function growShrinkLogo() {
+  var navBar = document.getElementById("navbar")
+  var logo = document.getElementById("logoId")
+
+
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+
+        navBar.className += " smallNavBar";
+        logo.style.width="100px";
+        logo.style.padding="5px 40px 15px 40px";
+
+          }
+     else {
+        navBar.className = "navbar";
+        logo.style.width="150px";
+        logo.style.padding=" 25px 20px 25px 20px";
+    }
+
+
+}
+
+*/
