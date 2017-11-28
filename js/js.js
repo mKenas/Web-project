5db0d5bc50d0b4d1a2ce35addd8b92b35b43b15f
Mohamad Kenas <m.kenas91@gmail.com>
2017-11-28 20:31:57 +0100

    Last changes

--- js/js.js
+++ js/js.js
@@ -1,87 +1,56 @@
-
 // This function repeat the carousel animation every 27 seconde.
 
-  setInterval(function(){
-  var container = document.getElementById('content');
-  var tmp = container.innerHTML;
-  container.innerHTML= tmp;
-}, 27500
-  );
+setInterval(function () {
+    var container = document.getElementById('carouselID');
+    var tmp = container.innerHTML;
+    container.innerHTML = tmp;
+}, 27500);
 
-// This function changes the class name of the navbar to apply the mobile style
-  function navbarMobile() {
+// This function changes the class name of the navbar to apply the mobile style to navbar.
+function navbarMobile() {
     var x = document.getElementById("navbar");
-
-    if (x.className === "navbar" ) {
+    if (x.className === "navbar") {
         x.className = "navbar mobileNavBar";
     } else {
         x.className = "navbar";
     }
-
-
-
-
 }
 
-
-
-function replayVideo(){
-  var icon = document.getElementById("replay-icon");
-  var replayText = document.getElementById("play-again-txt");
-  var video = document.getElementById("save-sharing-video");
-  if (video.paused){
-      video.currentTime = 0;
-      video.play();
-      icon.style.display = "none";
-      replayText.style.display = "none";
-
-
+// This function to replay the save-sharing-video.
+function replayVideo() {
+    var icon = document.getElementById("replay-icon");
+    var replayText = document.getElementById("play-again-txt");
+    var video = document.getElementById("save-sharing-video");
+    if (video.paused) {
+        video.currentTime = 0;
+        video.play();
+        icon.style.display = "none";
+        replayText.style.display = "none";
+    }
 }
 
-}
-function showReplayText(){
-var icon = document.getElementById("replay-icon");
-var replayText = document.getElementById("play-again-txt");
-icon.style.display = "block";
-replayText.style.display = "block";
-
-}
-
-
-
-window.onscroll = function() {
-
+// This function displays the replay text and icon when the video has played oance.
+function showReplayText() {
+    var icon = document.getElementById("replay-icon");
+    var replayText = document.getElementById("play-again-txt");
+    icon.style.display = "block";
+    replayText.style.display = "block";
 }
 
-
-function fadeIn() {
-
+// This function applies the animation to intro section.
+function introFadeIn() {
     var h1 = document.getElementById("intro-section-h1");
     var introTxt = document.getElementById("intro-section-text");
     h1.className = "intro-h1-animation";
-    introTxt.className = "intro-animation-txt";
-
+    introTxt.className = "intro-txt-animation";
 }
-/*
-function growShrinkLogo() {
-  var navBar = document.getElementById("navbar")
-  var logo = document.getElementById("logoId")
-
-
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-
-        navBar.className += " smallNavBar";
-        logo.style.width="100px";
-        logo.style.padding="5px 40px 15px 40px";
-
-          }
-     else {
-        navBar.className = "navbar";
-        logo.style.width="150px";
-        logo.style.padding=" 25px 20px 25px 20px";
-    }
-
-
+// This function applies the animation to how it works text.
+function howItWorksTxtFadeIn() {
+    var howItWorksTxt = document.getElementById("howItWorksTxtId");
+    howItWorksTxt.className = "how-it-works-txt-animation";
 }
-
-*/
+// This function applies the animation how it works image.
+function howItWorksImgFadeIn() {
+    var howItWorksImg = document.getElementById("howItWorksImgId");
+    howItWorksImg.className = "how-it-works-img-animation";
+}
